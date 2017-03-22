75c78fd3e62113ae2adb610f89059ddafda7d7cf
mrpinkcat <gatien.anizan@mail.com>
2017-03-22 23:13:56 +0000

    Addition of the typed.js script. Addition of the draft of the home page.

--- server/core.js
+++ server/core.js
@@ -2,7 +2,7 @@ exports.checkUsername = function(name) {
         if (name.length < 3 || name.indexOf(' ') > -1) {
             return 'error';
         }
-        if (name.length > 3) {
+        if (name.length >= 3) {
             return 'ok';
         }
 };
--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,7 @@
 var socket = io();
 
-var userId,
+var userName,
+    userId,
     gameId;
 
 $('#username-input').keypress(function(e) {
@@ -8,16 +9,31 @@ $('#username-input').keypress(function(e) {
         socket.emit('username', $('#username-input').val());
 
         socket.on('username-ok', function(newUserId) {
+            userName = $('#username-input').val();
             userId = newUserId;
             $('#username-input').val('');
-            $('#login-bc').fadeOut(500);
+            $('#login-bc').fadeOut(500, function() {
+                logOk();
+            });
         });
 
         socket.on('username-error', function() {
             $('#username-input').effect('shake');
-            $('#username-rules').css("color", "red");
-            $('#username-input').css("color", "red");
-            $('#username-input').css("border-bottom", "2px solid red");
+            $('#username-rules').css('color', 'red');
+            $('#username-input').css('color', 'red');
+            $('#username-input').css('border-bottom', '2px solid red');
         });
     }
 });
+
+function logOk() {
+    $('#menu').removeClass('invisible');
+    $('#main-title').typed({
+        strings: ['Bonjour commandant ' + userName + ' !'],
+        typeSpeed: 40,
+        showCursor: false,
+        callback: function() {
+            $('#cols').fadeIn(500);
+        }
+    });
+}
